37bafbc4eb206f8f6e2aabaea2ddc5298225f640
Kevin Vladimir Rodríguez Alvarado <00039425@uca.edu.sv>
2025-05-19 17:19:19 -0600

    Ejercicio de la serie de fibonacci terminado

---
+++ for/NFibonacci.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // The variables to use are declared
+    int NFibonacci = 0, n1 = 1, n2 = 1;
+
+    // The user is asked for the maximum number of terms
+    cout << "Enter the number of terms of the Fibonacci series you want to see: ";
+    cin >> NFibonacci;
+
+    for (int i = 0; i < NFibonacci; i++)
+    {
+        // The Fibonacci series is calculated starting with its first two values (1 and 1)
+        cout << n1 << " ";
+        int next = n1 + n2;
+        n1 = n2;
+        n2 = next;
+    }
+
+    return 0;
+}
