937c12235690a15666e3af20f4f54a796c7ea6b2
Kevin Vladimir Rodríguez Alvarado <00039425@uca.edu.sv>
2025-05-18 23:31:34 -0600

    Ahora se le pide el numero al usuario para iniciar la cuenta regresiva

--- for/CuentaRegresiva.cpp
+++ for/CuentaRegresiva.cpp
@@ -3,11 +3,20 @@
 using namespace std;
 
 int main(){
+    //Se declaran las variables a usar
+    int N;
+
+    //Se le pide el numero  al usuario
+    cout<< "Ingrese un numero para inicir la cuenta regresiva, por favor: ";
+    cin >> N;
+
+    cout << "\n---Cuenta regresiva---" << endl;
 
     //Se produce la cuenta regresiva
-    for (int i = 10; i >= 0; i--)
+    for (int i = N; i >= 0; i--)
     {
         //Se imprime la cuenta regresiva del 10 al 0
+        
         cout << i << endl;
     }
     
