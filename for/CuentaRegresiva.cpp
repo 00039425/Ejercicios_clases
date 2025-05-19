0464bf932d2782eb7b60c68a5a3a0f3670d339b9
Kevin Vladimir Rodríguez Alvarado <00039425@uca.edu.sv>
2025-05-18 23:25:53 -0600

    Ejercicio 5 de la carpeta de for hecho

---
+++ for/CuentaRegresiva.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    //Se produce la cuenta regresiva
+    for (int i = 10; i >= 0; i--)
+    {
+        //Se imprime la cuenta regresiva del 10 al 0
+        cout << i << endl;
+    }
+    
+
+    return 0;
+}
